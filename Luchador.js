b15d90eeae425ef062442e57397990a3c594596c
Menchu MV <mcmvillalobos@gmail.com>
2021-10-11 13:18:25 +0200

    Added custom properties to luchador class

--- Luchador.js
+++ Luchador.js
@@ -1,5 +1,12 @@
 import Personajes from "./Personajes.js";
 
-class Luchador extends Personajes {}
+class Luchador extends Personajes {
+  mensaje = "Primero pego y luego pregunto";
+  constructor(nombre, familia, edad, arma, destreza) {
+    super(nombre, familia, edad);
+    this.arma = arma;
+    this.destreza = destreza;
+  }
+}
 
 export default Luchador;
