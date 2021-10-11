6588f289d1d42b0067589c3b2e4f0c39746bd316
Menchu MV <mcmvillalobos@gmail.com>
2021-10-11 13:53:33 +0200

    Added jofferyBaratheon and jamieLannister objects

--- exercises.js
+++ exercises.js
@@ -5,4 +5,10 @@ import Asesor from "./Asesor.js";
 import Escudero from "./Escudero.js";
 
 const joffreyBaratheon = new Rey("Joffrey", "Lannister", 15, 1);
+const jamieLannister = new Luchador("Jamie", "Lannister", 32, "Espada", 7);
+
 console.log(joffreyBaratheon);
+console.log(joffreyBaratheon.comunicar());
+
+console.log(jamieLannister);
+console.log(jamieLannister.comunicar());
