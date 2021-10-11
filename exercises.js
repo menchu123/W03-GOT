5d843827451312f75b93bd700bf939bc2ebc2256
Menchu MV <mcmvillalobos@gmail.com>
2021-10-11 13:46:42 +0200

    Imported all other scripts into file

---
+++ exercises.js
@@ -0,0 +1,5 @@
+import Personajes from "./Personajes.js";
+import Rey from "./Rey.js";
+import Luchador from "./Luchador.js";
+import Asesor from "./Asesor.js";
+import Escudero from "./Escudero.js";
