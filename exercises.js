29d88a00e0b95ac819b1a57ff5679867121bd42d
Menchu MV <mcmvillalobos@gmail.com>
2021-10-11 13:49:24 +0200

    Updated html script to exercises.js

--- exercises.js
+++ exercises.js
@@ -3,3 +3,6 @@ import Rey from "./Rey.js";
 import Luchador from "./Luchador.js";
 import Asesor from "./Asesor.js";
 import Escudero from "./Escudero.js";
+
+const joffreyBaratheon = new Rey("Joffrey", "Lannister", 15, 1);
+console.log(joffreyBaratheon);
