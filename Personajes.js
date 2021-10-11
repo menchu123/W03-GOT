cfcb6a79f253e347a82645e733b29f5bbb56320d
Menchu MV <mcmvillalobos@gmail.com>
2021-10-11 12:36:18 +0200

    Added Personajes.js file

---
+++ Personajes.js
@@ -0,0 +1,13 @@
+class Personajes {
+  estado = "vivo";
+  serie = "Juego de Tronos";
+  mensaje;
+
+  constructor(nombre, familia, edad) {
+    this.nombre = nombre;
+    this.familia = familia;
+    this.edad = edad;
+  }
+}
+
+export default Personajes;
